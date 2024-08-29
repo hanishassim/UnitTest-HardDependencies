3a69700f714c4d3f4c8ca70f5553cff8009edba7
Hanis <hanishassim@users.noreply.github.com>
2024-08-29 09:44:31 +0800

    Add setUp and tearDown impl

--- HardDependenciesTests/MySingletonViewControllerTests.swift
+++ HardDependenciesTests/MySingletonViewControllerTests.swift
@@ -2,7 +2,20 @@
 import XCTest
 
 final class MySingletonViewControllerTests: XCTestCase {
-    func test_zero() throws {
-        XCTFail("Tests not yet implemented in MySingletonViewControllerTests")
+    override func setUp() {
+        super.setUp()
+        MySingletonAnalytics.stubbedInstance = MySingletonAnalytics()
+    }
+    
+    override class func tearDown() {
+        MySingletonAnalytics.stubbedInstance = nil
+        super.tearDown()
+    }
+    
+    func test_viewDidAppear() {
+        let sut = MySingletonViewController()
+        sut.loadViewIfNeeded()
+        
+        sut.viewDidAppear(false)
     }
 }
